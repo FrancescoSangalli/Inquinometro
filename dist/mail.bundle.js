6105aeaa2b85c54acbccbeea39f05526e7c48240
FrancescoSangalli <ilgallofranc@gmail.com>
2021-09-29 14:01:08 +0200

    dist

---
+++ dist/mail.bundle.js
@@ -0,0 +1,1 @@
+window.onload=function(){document.getElementById("contact-form").addEventListener("submit",(function(e){e.preventDefault(),this.user_name.value,this.user_email.value,this.message.value,emailjs.sendForm("service_sm5qyia","contact_form",this).then((function(){console.log("SUCCESS!")}),(function(e){console.log("FAILED...",e)}))}))};
